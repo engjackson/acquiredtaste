39ca5a4aeada06d3c5dee151a2cd6aed090395be
Jackson Eng <jacksoneng@Jacksons-MacBook-Air.local>
2025-11-11 08:55:03 -0500

    feat: Add Google Analytics tracking code via next/script

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,7 +12,27 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
+        <html lang="en">
+      {/* 2. Add the first part of the GA script inside the <head> using <Script> */}
+      <Script 
+        strategy="afterInteractive" // Load script after the page becomes interactive
+        src="https://www.googletagmanager.com/gtag/js?id=G-BW34P93HJS"
+      />
+      
+      {/* 3. Add the second part of the GA script using another <Script> tag */}
+      <Script 
+        id='google-analytics-config' // Give it a unique ID
+        strategy="afterInteractive" 
+      >
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'G-BW34P93HJS');
+        `}
+      </Script>
+
       <body className="antialiased">{children}</body>
     </html>
   );
