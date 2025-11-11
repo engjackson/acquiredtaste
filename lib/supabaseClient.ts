da04c2a3d9269904e5c4f513de072acef44ff9b8
Jackson Eng <jacksoneng@Jacksons-MacBook-Air.local>
2025-11-10 23:27:29 -0500

    fix: Hardcoded Supabase public keys for static deployment

--- lib/supabaseClient.ts
+++ lib/supabaseClient.ts
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
+const supabaseUrl = "https://yiccjijuhnyjmfihawcc.supabase.co"; 
+const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InlpY2NqaWp1aG55am1maWhhd2NjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjE5MTEwODMsImV4cCI6MjA3NzQ4NzA4M30.OqwVK_tgSq7qPRc-VqziSh9JIgOg6M_xmB2xExUVgd8"; 
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
