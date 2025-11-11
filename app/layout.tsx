b3b42a67d180708e231b4999ce662e8266892b87
Jackson Eng <jacksoneng@Jacksons-MacBook-Air.local>
2025-11-11 10:52:19 -0500

    feat: Fixed Mixpanel implementation in layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,16 +13,14 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-        <html lang="en">
-      {/* 2. Add the first part of the GA script inside the <head> using <Script> */}
+    <html lang="en">
+      {/* GA Scripts (Unchanged) */}
       <Script 
-        strategy="afterInteractive" // Load script after the page becomes interactive
+        strategy="afterInteractive" 
         src="https://www.googletagmanager.com/gtag/js?id=G-BW34P93HJS"
       />
-      
-      {/* 3. Add the second part of the GA script using another <Script> tag */}
       <Script 
-        id='google-analytics-config' // Give it a unique ID
+        id='google-analytics-config'
         strategy="afterInteractive" 
       >
         {`
@@ -34,29 +32,27 @@ export default function RootLayout({
         `}
       </Script>
 
+      {/* MIXPANEL SCRIPT - **THE FIX IS APPLIED HERE** */}
+      <Script
+        id="mixpanel-config"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            (function(e,c){if(!c.__SV){var l,h;window.mixpanel=c;c._i=[];c.init=function(q,r,f){function t(d,a){var g=a.split(".");2==g.length&&(d=d[g[0]],a=g[1]);d[a]=function(){d.push([a].concat(Array.prototype.slice.call(arguments,0)))}}var b=c;"undefined"!==typeof f?b=c[f]=[]:f="mixpanel";b.people=b.people||[];b.toString=function(d){var a="mixpanel";"mixpanel"!==f&&(a+="."+f);d||(a+=" (stub)");return a};b.people.toString=function(){return b.toString(1)+".people (stub)"};l="disable time_event track track_pageview track_links track_forms track_with_groups add_group set_group remove_group register register_once alias unregister identify name_tag set_config reset opt_in_tracking opt_out_tracking has_opted_in_tracking has_opted_out_tracking clear_opt_in_out_tracking start_batch_senders start_session_recording stop_session_recording people.set people.set_once people.unset people.increment people.append people.union people.track_charge people.clear_charges people.delete_user people.remove".split(" ");
+            for(h=0;h<l.length;h++)t(b,l[h]);var n="set set_once union unset remove delete".split(" ");b.get_group=function(){function d(p){a[p]=function(){b.push([g,[p].concat(Array.prototype.slice.call(arguments,0))])}}for(var a={},g=["get_group"].concat(Array.prototype.slice.call(arguments,0)),m=0;m<n.length;m++)d(n[m]);return a};c._i.push([q,r,f])};c.__SV=1.2;var k=e.createElement("script");k.type="text/javascript";k.async=!0;k.src="undefined"!==typeof MIXPANEL_CUSTOM_LIB_URL?MIXPANEL_CUSTOM_LIB_URL:"file:"===
+            e.location.protocol&&"//cdn.mxpnl.com/libs/mixpanel-2-latest.min.js".match(/^\/\/\//)?"https://cdn.mxpnl.com/libs/mixpanel-2-latest.min.js":"//cdn.mxpnl.com/libs/mixpanel-2-latest.min.js";e=e.getElementsByTagName("script")[0];e.parentNode.insertBefore(k,e)}})(document,window.mixpanel||[])
 
-<Script
-  id="mixpanel-config"
-  strategy="afterInteractive"
-  // ⭐ FIX: Use dangerouslySetInnerHTML for reliable execution
-  dangerouslySetInnerHTML={{
-    __html: `
-      (function(e,c){if(!c.__SV){var l,h;window.mixpanel=c;c._i=[];c.init=function(q,r,f){function t(d,a){var g=a.split(".");2==g.length&&(d=d[g[0]],a=g[1]);d[a]=function(){d.push([a].concat(Array.prototype.slice.call(arguments,0)))}}var b=c;"undefined"!==typeof f?b=c[f]=[]:f="mixpanel";b.people=b.people||[];b.toString=function(d){var a="mixpanel";"mixpanel"!==f&&(a+="."+f);d||(a+=" (stub)");return a};b.people.toString=function(){return b.toString(1)+".people (stub)"};l="disable time_event track track_pageview track_links track_forms track_with_groups add_group set_group remove_group register register_once alias unregister identify name_tag set_config reset opt_in_tracking opt_out_tracking has_opted_in_tracking has_opted_out_tracking clear_opt_in_out_tracking start_batch_senders start_session_recording stop_session_recording people.set people.set_once people.unset people.increment people.append people.union people.track_charge people.clear_charges people.delete_user people.remove".split(" ");
-      for(h=0;h<l.length;h++)t(b,l[h]);var n="set set_once union unset remove delete".split(" ");b.get_group=function(){function d(p){a[p]=function(){b.push([g,[p].concat(Array.prototype.slice.call(arguments,0))])}}for(var a={},g=["get_group"].concat(Array.prototype.slice.call(arguments,0)),m=0;m<n.length;m++)d(n[m]);return a};c._i.push([q,r,f])};c.__SV=1.2;var k=e.createElement("script");k.type="text/javascript";k.async=!0;k.src="undefined"!==typeof MIXPANEL_CUSTOM_LIB_URL?MIXPANEL_CUSTOM_LIB_URL:"file:"===
-      e.location.protocol&&"//cdn.mxpnl.com/libs/mixpanel-2-latest.min.js".match(/^\/\//)?"https://cdn.mxpnl.com/libs/mixpanel-2-latest.min.js":"//cdn.mxpnl.com/libs/mixpanel-2-latest.min.js";e=e.getElementsByTagName("script")[0];e.parentNode.insertBefore(k,e)}})(document,window.mixpanel||[])
-
-      mixpanel.init('2804354f340e1aad20d305f676987e0c', {
-        autocapture: true,
-        record_sessions_percent: 100,
-        debug: true, // <-- IMPORTANT: Turn on debugging
-      })
-    `,
-  }}
-/>
+            mixpanel.init('2804354f340e1aad20d305f676987e0c', {
+              autocapture: true,
+              record_sessions_percent: 100,
+              debug: true,
+            })
+          `,
+        }}
+      />
 
 
       <body className="antialiased">{children}</body>
     </html>
   );
 }
-
